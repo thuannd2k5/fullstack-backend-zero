97c2c589386b48c53512ba34caf774fa30fd369c
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-19 20:41:09 +0700

    code bất đồng bộ

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,22 +5,30 @@ const HomeController = (req, res) => {
 
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
 
     let email = req.body.email;
     let name = req.body.name;
     let city = req.body.city;
 
-    connection.query(
-        `INSERT INTO Users (email, name, city)
-        VALUES (?, ?,?)`,
-        [email, name, city],
-        function (err, results) {
-            res.send("create user success")
-        }
-    );
+    // connection.query(
+    //     `INSERT INTO Users (email, name, city)
+    //     VALUES (?, ?,?)`,
+    //     [email, name, city],
+    //     function (err, results) {
+    //         res.send("create user success")
+    //     }
+    // );
+    const [results, fields] = await connection.query(
+        `INSERT INTO Users (email, name, city)VALUES (?, ?,?)`, [email, name, city],);
+
+    res.send("create user success")
+}
+
+const getCreatePage = (req, res) => {
+    res.render('create')
 }
 
 module.exports = {
-    HomeController, postCreateUser
+    HomeController, postCreateUser, getCreatePage
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { HomeController, postCreateUser } = require('../controllers/homeController')
+const { HomeController, postCreateUser, getCreatePage } = require('../controllers/homeController')
 const router = express.Router()
 
 
 router.get('/', HomeController);
-
+router.get('/create', getCreatePage)
 router.post('/create-user', postCreateUser)
+
 
 module.exports = router;
