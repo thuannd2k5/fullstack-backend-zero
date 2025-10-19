80c8f3dd7eafd7b9023d176123a77c6ee6ea71c3
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-19 10:31:26 +0700

    kết nối với db sử dụng dbeaver và docker

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
-
+const mysql = require('mysql2');
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -13,6 +13,24 @@ configViewEngine(app);
 
 //khai báo routes
 app.use("/", webRoutes);
+
+
+//test connection
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root',
+    password: '123456',
+    database: 'hoidanit'
+});
+connection.query(
+    'SELECT * from Users u ;',
+    function (err, results, fields) {
+        console.log("results >>> : ", results); // results contains rows returned by server
+        console.log("fields >>>: ", fields); // fields contains extra meta data about results, if available
+    }
+);
 
 
 app.listen(port, hostname, () => {
