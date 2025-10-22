f2260f0c71b5fab3b0d1f08e057374a858fd1c60
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-23 00:21:16 +0700

    bước đầu tạo db, model

--- src/config/database.js
+++ src/config/database.js
@@ -3,31 +3,31 @@ require('dotenv').config()
 
 const dbState = [{
     value: 0,
-    label: "disconnected"
+    label: "Disconnected"
 },
 {
     value: 1,
-    label: "connected"
+    label: "Connected"
 },
 {
     value: 2,
-    label: "connecting"
+    label: "Connecting"
 },
 {
     value: 3,
-    label: "disconnecting"
+    label: "Disconnecting"
 }];
 
 
 const connection = async () => {
-    // Or:
-    try {
-        await mongoose.connect('mongodb://root:123456@localhost:27018/?authSource=admin');
-        const state = Number(mongoose.connection.readyState);
-        console.log(dbState.find(f => f.value == state).label, "to db"); // connected to db
-    } catch (error) {
-        console.log(">>> Error connection DB : ", error)
+    const options = {
+        user: process.env.DB_USER,
+        pass: process.env.DB_PASSWORD,
+        dbName: process.env.DB_NAME
     }
+    await mongoose.connect(process.env.DB_HOST, options);
+    const state = Number(mongoose.connection.readyState);
+    console.log(dbState.find(f => f.value === state).label, "to database"); // connected to db
 }
 
 module.exports = connection;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web');
 const connection = require('./config/database');
+const { default: mongoose } = require('mongoose');
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -18,8 +19,22 @@ app.use(express.urlencoded({ extended: true })) // for form data
 //khai báo routes
 app.use("/", webRoutes);
 
-connection();
+const kittySchema = new mongoose.Schema({
+    name: String
+});
+const Kitten = mongoose.model('Kitten', kittySchema);
+const cat = new Kitten({ name: 'thuan' });
+cat.save();
 
-app.listen(port, hostname, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+(async () => {
+    try {
+        await connection();
+        app.listen(port, hostname, () => {
+            console.log(`back end zero app listening on port ${port}`)
+        })
+    } catch (error) {
+        console.log(">>> Error connect to DB : ", error)
+    }
+})()
+
+
