d8719f0e00609b3e88223ea4a86f1e50ca9a7c50
thuannd2k5 <nguyenthuan22112005@gmail.com>
2025-10-25 21:44:16 +0700

    kết nối mongodb driver và read/write

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const webRoutes = require('./routes/web');
 const APIRoutes = require('./routes/api');
 const connection = require('./config/database');
 const fileUpload = require('express-fileupload');
-
+const { MongoClient } = require('mongodb');
 
 const app = express()
 const port = process.env.PORT || 8888;
@@ -29,7 +29,26 @@ app.use("/v1/api", APIRoutes);
 
 (async () => {
     try {
-        await connection();
+        //using mongoose
+        // await connection();
+
+        //using mongodb driver
+        // Connection URL
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+
+        // Database Name
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('customers');
+
+        // collection.insertOne({ "name": "nguyen thuan" })
+        console.log(">>> find = ", await collection.findOne({ address: "hcm" }))
+
+        //
         app.listen(port, hostname, () => {
             console.log(`back end zero app listening on port ${port}`)
         })
